a4fb3cda9b47f1369f92c6093eb738ea6c908a19
Harsh Sharma <133791781+HarshSharmaPython@users.noreply.github.com>
2024-10-07 11:42:32 +0530

    testing

--- server/api/orders.ts
+++ server/api/orders.ts
@@ -1,18 +1,18 @@
-import { defineEventHandler } from 'h3';
-import fetch from 'node-fetch';
+import { defineEventHandler } from "h3";
+import fetch from "node-fetch";
 
-const shopDomain = 'dev-mediu.myshopify.com';
-const accessToken = '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';
+const shopDomain = "admin.shopify.com";
+const accessToken = "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";
 
 export default defineEventHandler(async () => {
   const apiUrl = `https://${shopDomain}/admin/api/2024-07/orders.json?status=any`;
 
   try {
     const response = await fetch(apiUrl, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
-        'X-Shopify-Access-Token': accessToken,
+        "Content-Type": "application/json",
+        "X-Shopify-Access-Token": accessToken,
       },
     });
 
