5549644bda55abdff9051f78594928bf0298da11
compitcomds <compitcomds@gmail.com>
2024-10-08 13:16:26 +0530

    this is ypdate push by DJ and nuxtconfig is been altered

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,33 @@
+// // https://nuxt.com/docs/api/configuration/nuxt-config
+// export default defineNuxtConfig({
+//   compatibilityDate: '2024-04-03',
+//   devtools: { enabled: true },
+//   css: ['~/assets/css/main.css'],
+//   plugins: [
+//     '~/plugins/appwrite.js'
+//   ],
+//   postcss: {
+//     plugins: {
+//       tailwindcss: {},
+//       autoprefixer: {},
+//     },
+//   },
+//   nitro: {
+//     preset: 'node-server',
+//   },
+
+//   runtimeConfig: {
+//     public: {
+//       shopifyStoreDomain: "https://dev-mediu.myshopify.com/",
+//       shopifyApiVersion: "2024-04",
+//       shopifyPublicAccessToken: "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",
+//       // baseURL: "http://localhost:3001",
+//       baseURL:" https://admin.mediu.in/"
+//     },
+//   },
+// })
+
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
@@ -14,6 +44,11 @@ export default defineNuxtConfig({
   },
   nitro: {
     preset: 'node-server',
+    routeRules: {
+      '/api/**': {
+        cors: true
+      }
+    }
   },
 
   runtimeConfig: {
@@ -21,10 +56,7 @@ export default defineNuxtConfig({
       shopifyStoreDomain: "https://dev-mediu.myshopify.com/",
       shopifyApiVersion: "2024-04",
       shopifyPublicAccessToken: "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",
-
-      
-      // baseURL: "http://localhost:3001",
-      baseURL:" https://admin.mediu.in/"
+      baseURL: " https://admin.mediu.in/"
     },
   },
 })
