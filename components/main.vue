8420bee660bdf789bbc486cc9e048394cd684585
compitcomds <compitcomds@gmail.com>
2024-10-08 14:04:39 +0530

    fghdf

--- components/main.vue
+++ components/main.vue
@@ -1,12 +1,10 @@
 <template>
-  <div class="lg:ml-64 flex-1 items-center justify-center overflow-y-auto p-8 mt-10 bg-gray-100">
-    <div class="bg-white flex shadow-md rounded p-6 mb-4">
-      <h2 class="text-4xl text-center font-bold mb-4">Welcome To Mediu</h2>
-      
-    </div>
-
-    <!-- Additional Sections -->
-    
+  <div class="lg:ml-64 flex-1 w-full items-center justify-center overflow-y-auto mt-10 bg-gray-100">
+    <div class="bg-white flex flex-col shadow-md rounded p-6 mb-4 h-screen items-center justify-center space-y-4 w-full text-[#28574e]">
+      <img src="https://ccdstest.b-cdn.net/Medi%20u/logo%202.png" alt="" class="w-80">
+      <h2 class="text-6xl text-center font-semibold mb-4">Hey Admin</h2>
+      <h2 class="text-6xl text-center font-semibold mb-4">Welcome To Mediu</h2>
+    </div>    
   </div>
 </template>
 
