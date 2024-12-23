6d7be1c6c84634daef5f2604a3bf7eed5d739cc4
Vikas Maur <vikasmaur10@gmail.com>
2024-12-23 16:14:06 +0530

    console logging url, shiprocket password and email

--- server/api/shiprocket/access-token/index.get.ts
+++ server/api/shiprocket/access-token/index.get.ts
@@ -6,6 +6,9 @@ const SHIPROCKET_PASSWORD = process.env.SHIPROCKET_PASSWORD;
 
 export default defineEventHandler(async (event) => {
   const url = `${SHIPROCKET_API_URL}/auth/login`;
+  console.log(url);
+  console.log(SHIPROCKET_PASSWORD);
+  console.log(SHIPROCKET_EMAIL);
   const { data } = await axios.post(
     url,
     { email: SHIPROCKET_EMAIL, password: SHIPROCKET_PASSWORD },
