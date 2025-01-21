22a029d0a6da096b8341a6ebef2f70145b6ec0e2
Vikas Maur <vikasmaur10@gmail.com>
2025-01-22 01:22:28 +0530

    added webhook authorization

--- server/api/confirm-cod-payment/index.post.ts
+++ server/api/confirm-cod-payment/index.post.ts
@@ -1,5 +1,8 @@
 import shopifyClient from "~/server/helpers/shopify-graphql-client";
 
+const SHIPROCKET_CONFIRM_COD_TOKEN = process.env
+  .SHIPROCKET_CONFIRM_COD_TOKEN as string;
+
 const orderMarkAsPaidMutation = `
 mutation markOrderAsPaid($orderId: ID!) {
   orderMarkAsPaid(input: {id: $orderId}) {
@@ -12,6 +15,10 @@ mutation markOrderAsPaid($orderId: ID!) {
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
+  const header = getHeader(event, "x-api-key");
+
+  if (header !== SHIPROCKET_CONFIRM_COD_TOKEN) return;
+
   console.log(body.current_status);
   console.log(body);
   if (body.current_status.toLowerCase() !== "delivered") return;
